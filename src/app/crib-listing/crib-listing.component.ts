2adbfcff6a83c7f356d3e176eaf34fb6a870766d
Lucas Sena <lucassena.rj@gmail.com>
2017-08-10 13:58:46 -0300

    Implements http requests
    Using http request to get json datas.

--- src/app/crib-listing/crib-listing.component.ts
+++ src/app/crib-listing/crib-listing.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { cribs } from './../data/cribs';
+import { Http } from '@angular/http';
+import { CribsService } from './../services/cribs.service';
 
 @Component({
   selector: 'app-crib-listing',
@@ -8,11 +9,21 @@ import { cribs } from './../data/cribs';
 })
 export class CribListingComponent implements OnInit {
 
-  cribs: Array<any> = cribs;
+  cribs: Array<any>;
+  error: string;
 
-  constructor() { }
+  constructor(
+    private http: Http, 
+    private cribsServices: CribsService
+  ) { }
 
   ngOnInit() {
+    this.cribsServices.getAllCribs()
+      .subscribe(
+        data => this.cribs = data,
+        error => this.error = error.statusText
+      );
+    
   }
 
 }
